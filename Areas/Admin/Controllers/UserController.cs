5fb9a7732f2e1c8f0f719468db704f3adba82e6c
pouya sharifi <pouyasharifi99@gmail.com>
2024-08-05 14:32:36 +0330

    Add Area

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MvcBuggetoEx.Areas.Admin.Models;
+using MvcBuggetoEx.Models;
+using MvcBuggetoEx.Models.DTO;
+
+namespace MvcBuggetoEx.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users
+                .Select(p => new UserListDto
+                {
+                    Id = p.Id,
+                    FirstName = p.FirstName,
+                    LastName = p.LastName,
+                    UserName = p.UserName,
+                    PhoneNumber = p.PhoneNumber,
+                    EmailConfirmed = p.EmailConfirmed,
+                    AccessFailedCount = p.AccessFailedCount
+                }).ToList();
+                  return View(users); 
+                
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(RegisterDto register)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(register);
+            }
+
+            User newUser = new User()
+            {
+                FirstName = register.FirstName,
+                LastName = register.LastName,
+                Email = register.Email,
+                UserName = register.Email,
+            };
+
+            var result = _userManager.CreateAsync(newUser, register.Password).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "user", new { area = "admin" });
+            }
+
+            string message = "";
+            foreach (var item in result.Errors.ToList())
+            {
+                message += item.Description + Environment.NewLine;
+            }
+            TempData["Message"] = message;
+
+            return View(register);
+        }
+    }
+}
---
+++ Areas/Admin/Models/UserDeleteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcBuggetoEx.Areas.Admin.Models
+{
+    public class UserDeleteDto
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
